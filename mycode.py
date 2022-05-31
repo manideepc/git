4f55e1d576af5176b332aeae0e595a8aeebcae9a
Manideep Chenna <36693990+manideepc@users.noreply.github.com>
2022-05-31 14:39:18 +0530

    Add files via upload

---
+++ mycode.py
@@ -0,0 +1,11 @@
+def addition(a,b):
+    return a+b
+def substraction(a,b):
+    return a-b
+def multiplication(a,b):
+    return a*b  
+def division(a,b):      
+    if b==0:
+        return "Error"
+    return a//b
+
---
+++ test_my_code.py
@@ -0,0 +1,7 @@
+from mycode import *
+a=11
+b=2
+print(addition(a,b))
+print(substraction(a,b))
+print(multiplication(a,b))
+print(division(a,b))
